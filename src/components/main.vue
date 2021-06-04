c2c4aa9eca8de5e03b8c13628465ed48189d95d1
Kadeluxe <63540878+Kadeluxe@users.noreply.github.com>
2021-06-05 00:01:01 +0300

    Initial commit

---
+++ src/components/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="~src/images/150.png"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  setup(props, ctx) {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
